0cb9902d513ada85b64ad5f4fef25bdde30cc03a
ofitz <ofitz@outlook.de>
2016-12-02 09:46:41 +0100

    init

---
+++ app/src/main/java/wifirelay/esp8266/ofi/esp8266/MainActivity.java
@@ -0,0 +1,13 @@
+package wifirelay.esp8266.ofi.esp8266;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity_layout);
+    }
+}
